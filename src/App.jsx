c794d72c8f02c6ebe8bdd7fa649f679be2f0eba1
kamil-homer <kamilhomer@gmail.com>
2024-06-18 22:31:47 +0200

    mui + prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Button variant="contained">Hello world</Button>;
       </div>
       <h1>Vite + React</h1>
       <div className="card">
