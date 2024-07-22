c28b310e5ee35319abe39134b972fced2bca9a75
kamil-homer <kamilhomer@gmail.com>
2024-07-22 15:16:08 +0200

    add newReadingForm

--- src/components/NewReadingForm/NewReadingForm.jsx
+++ src/components/NewReadingForm/NewReadingForm.jsx
@@ -1,6 +1,24 @@
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, TextField, Typography } from '@mui/material'
+import { useState } from 'react'
 
 export const NewReadingForm = () => {
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: '',
+    currentPage: '',
+  })
+
+  const handleChange = (e, fieldName) => {
+    setNewBook((currentState) => ({
+      ...currentState,
+      [fieldName]: e.target.value,
+    }))
+  }
+
+  const handleSubmit = () => {
+    console.log('New reading: ', newBook)
+  }
+
   return (
     <>
       <Grid
@@ -12,8 +30,33 @@ export const NewReadingForm = () => {
           Add New Reading
         </Typography>
       </Grid>
-      <Grid container justifyContent="space-between">
-        {/* TODO: add form */}
+      <Grid container direction="column" justifyContent="space-between" gap={1}>
+        <TextField
+          id="new-book-title"
+          label="Title"
+          variant="outlined"
+          value={newBook.title}
+          onChange={(e) => handleChange(e, 'title')}
+        />
+        <TextField
+          id="new-book-author"
+          label="Author"
+          variant="outlined"
+          value={newBook.author}
+          onChange={(e) => handleChange(e, 'author')}
+        />
+        <TextField
+          id="new-book-current-page"
+          label="Current page"
+          variant="outlined"
+          value={newBook.currentPage}
+          onChange={(e) => {
+            if (!isNaN(e.target.value)) {
+              handleChange(e, 'currentPage')
+            }
+          }}
+        />
+        <Button onClick={handleSubmit}>Add New Reading</Button>
       </Grid>
     </>
   )
